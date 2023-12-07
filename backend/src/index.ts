81f7cab4fa838f2ace9d831fd4c35fee65d69eaa
Nanret123 <nanretgungshik@gmail.com>
2023-12-07 03:04:12 +0100

    corrections

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,12 +17,13 @@ mongoose.set("strictQuery", true);
 
 const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'https://typescript-ecommerce-frontend.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+const allowedOrigins = ["https://typescript-ecommerce-frontend.vercel.app"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
