0c72d9cd3aab9adbfc167d1459cb3c12af948e83
Nanret123 <nanretgungshik@gmail.com>
2023-12-07 02:54:41 +0100

    corrections

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,8 +18,11 @@ mongoose.set("strictQuery", true);
 const app = express();
 
 app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', 'https://typescript-ecommerce-frontend.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-}, cors({ maxAge: 84600 }));
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
